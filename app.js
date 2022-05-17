6b1fed1e6e2798d4954f6574221b3d734735909d
unknown <thomascarreno02@gmail.com>
2022-05-17 16:00:29 -0300

    Thomas Carreño

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var close = document.querySelector(".close")
 var open = document.querySelector(".open")
 var popUp = document.querySelector(".popUp")
 var popUpContainer = document.querySelector(".popUp-container")
+var lateralInfo = document.querySelector(".lateralInfo")
+
+
+var activeLateral = document.querySelector('.hambur')
+var desactivarLateral = document.querySelector('.desactivar')
 
 
 open.addEventListener('click', function(e) {
@@ -30,4 +35,19 @@ window.addEventListener('click',function(e){
             popUpContainer.style.visibility ='hidden';
         },320)
     }
+})
+
+
+activeLateral.addEventListener('click',function(){
+    lateralInfo.classList.toggle('activo')
+    activeLateral.style.opacity='0'
+    activeLateral.style.visibility='hidden'
+
+})
+
+desactivarLateral.addEventListener('click',function(){
+    lateralInfo.classList.toggle('activo')
+    activeLateral.style.opacity='1'
+    activeLateral.style.visibility='visible'
+
 })
