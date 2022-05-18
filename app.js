b75a705f60c34e369e33a56c4f2ae34a4e3cd659
unknown <thomascarreno02@gmail.com>
2022-05-18 16:10:17 -0300

    Thomas Carreño

--- app.js
+++ app.js
@@ -1,53 +1,46 @@
-var close = document.querySelector(".close")
-var open = document.querySelector(".open")
-var popUp = document.querySelector(".popUp")
-var popUpContainer = document.querySelector(".popUp-container")
-var lateralInfo = document.querySelector(".lateralInfo")
+var close = document.querySelector(".close");
+var open = document.querySelector(".open");
+var popUp = document.querySelector(".popUp");
+var popUpContainer = document.querySelector(".popUp-container");
+var lateralInfo = document.querySelector(".lateralInfo");
 
+var activeLateral = document.querySelector(".hambur");
+var desactivarLateral = document.querySelector(".desactivar");
 
-var activeLateral = document.querySelector('.hambur')
-var desactivarLateral = document.querySelector('.desactivar')
-
-
-open.addEventListener('click', function(e) {
-    e.preventDefault()
-    popUpContainer.style.opacity ='1';
-    popUpContainer.style.visibility ='visible';
-    popUp.classList.toggle('popUp-close')
-
+open.addEventListener("click", function (e) {
+  e.preventDefault();
+  popUpContainer.style.opacity = "1";
+  popUpContainer.style.visibility = "visible";
+  popUp.classList.toggle("popUp-close");
 });
 
-close.addEventListener('click', function(e) {
-    e.preventDefault()
-    popUp.classList.toggle('popUp-close')
-    setTimeout(()=>{
-        popUpContainer.style.opacity ='0';
-        popUpContainer.style.visibility ='hidden';
-    },320)
+close.addEventListener("click", function (e) {
+  e.preventDefault();
+  popUp.classList.toggle("popUp-close");
+  setTimeout(() => {
+    popUpContainer.style.opacity = "0";
+    popUpContainer.style.visibility = "hidden";
+  }, 320);
+});
 
-})
+window.addEventListener("click", function (e) {
+  if (e.target == popUpContainer) {
+    popUp.classList.toggle("popUp-close");
+    setTimeout(() => {
+      popUpContainer.style.opacity = "0";
+      popUpContainer.style.visibility = "hidden";
+    }, 320);
+  }
+});
 
-window.addEventListener('click',function(e){
-    if(e.target == popUpContainer){
-        popUp.classList.toggle('popUp-close')
-        setTimeout(()=>{
-            popUpContainer.style.opacity ='0';
-            popUpContainer.style.visibility ='hidden';
-        },320)
-    }
-})
+activeLateral.addEventListener("click", function () {
+  lateralInfo.classList.toggle("activo");
+  activeLateral.style.opacity = "0";
+  activeLateral.style.visibility = "hidden";
+});
 
-
-activeLateral.addEventListener('click',function(){
-    lateralInfo.classList.toggle('activo')
-    activeLateral.style.opacity='0'
-    activeLateral.style.visibility='hidden'
-
-})
-
-desactivarLateral.addEventListener('click',function(){
-    lateralInfo.classList.toggle('activo')
-    activeLateral.style.opacity='1'
-    activeLateral.style.visibility='visible'
-
-})
+desactivarLateral.addEventListener("click", function () {
+  lateralInfo.classList.toggle("activo");
+  activeLateral.style.opacity = "1";
+  activeLateral.style.visibility = "visible";
+});
